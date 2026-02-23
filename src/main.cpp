3af54aa7a1feec8b2eb6c9e7f42e6a2af6b5b03e
kayvan-faghani <kayfaghani@gmail.com>
2026-02-23 14:50:47 +0000

    New branch

--- src/main.cpp
+++ src/main.cpp
@@ -351,13 +351,20 @@ void scanKeysTask(void * pvParameters) {
 
         // Key scanning loop for Rows 0-2
         std::bitset<32> localInputs;
-        for (int i = 0; i < 7; i++) {
-            if (i == 3 || i == 4) continue; // Skip rotation rows now on I2C  
+        for (int i = 0, offset = 0; i < 7; i++) { 
             
             setRow(i);
             delayMicroseconds(3);
             std::bitset<4> cols = readCols();
 
+            if (3 <= i  && i < 5) {
+                uint8_t knobIndex = (i == 3) ? 3 : 1;
+                knobs[knobIndex].setInitialState(sysState.inputs[0+offset], sysState.inputs[1+offset]);
+                knobs[knobIndex].setInitialState(sysState.inputs[2+offset], sysState.inputs[3+offset]);
+                knobs[knobIndex].updateRotation(cols[0],cols[1]);
+                knobs[knobIndex-1].updateRotation(cols[2],cols[3]);
+                continue;
+            }
             // Map rows 5 and 6 to knob switches
             // and updates east and west connections
             updateSwitchesAndConnections(cols, i, westConnected, eastConnected);
@@ -629,12 +636,12 @@ void initialiseHardwareTimer() {
 void initialiseThreads() {
     #ifndef DISABLE_THREADS
     TaskHandle_t scanKeysHandle = NULL;
-    TaskHandle_t knobHandle = NULL;
+    // TaskHandle_t knobHandle = NULL;
     TaskHandle_t decodeHandle = NULL;
     TaskHandle_t displayUpdateHandle = NULL;
     TaskHandle_t canTxHandle = NULL;
     xTaskCreate(scanKeysTask, "scanKeys", 128, NULL, 3, &scanKeysHandle);
-    xTaskCreate(knobTask, "knob", 128, NULL, 4, &knobHandle);
+    // xTaskCreate(knobTask, "knob", 128, NULL, 4, &knobHandle);
     xTaskCreate(displayUpdateTask, "displayUpdate", 256, NULL, 1, &displayUpdateHandle);
     xTaskCreate(decodeTask, "decode", 128, NULL, 2, &decodeHandle);
     xTaskCreate(CAN_TX_Task, "canTX", 128, NULL, 2, &canTxHandle);
