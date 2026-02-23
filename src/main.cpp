08d8f399062303a8ef9638396f771c0ca90d65ee
kayvan-faghani <kayfaghani@gmail.com>
2026-02-23 16:11:19 +0000

    working

--- src/main.cpp
+++ src/main.cpp
@@ -351,21 +351,17 @@ void scanKeysTask(void * pvParameters) {
 
         // Key scanning loop for Rows 0-2
         std::bitset<32> localInputs;
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 7; i++) { 
             
             setRow(i);
             delayMicroseconds(3);
             std::bitset<4> cols = readCols();
 
-            if (i == 3) {
-                knobs[0].updateRotation(cols[0], cols[1]);
-                knobs[1].updateRotation(cols[2], cols[3]);
+            if (3 <= i  && i < 5) {
+                uint8_t knobIndex = (i == 3) ? 3 : 1;
+                knobs[knobIndex].updateRotation(cols[0],cols[1]);
+                knobs[knobIndex-1].updateRotation(cols[2],cols[3]);
             }
-            if (i == 4) {
-                knobs[2].updateRotation(cols[0], cols[1]);
-                knobs[3].updateRotation(cols[2], cols[3]);
-            }
-
             // Map rows 5 and 6 to knob switches
             // and updates east and west connections
             updateSwitchesAndConnections(cols, i, westConnected, eastConnected);
